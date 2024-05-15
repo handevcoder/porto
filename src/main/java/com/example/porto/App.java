68e63edc2ee8e33af56aff3921fd0aa2a54734f7
handevcoder <iwansyy@gmail.com>
2024-05-15 23:17:26 +0700

    - Add Figlet just for fun

--- src/main/java/com/example/porto/App.java
+++ src/main/java/com/example/porto/App.java
@@ -1,39 +1,37 @@
 package com.example.porto;
 
-import com.example.porto.mvc.model.Role;
-import com.example.porto.mvc.model.User;
 import com.example.porto.mvc.repository.UserRepository;
+import com.github.lalyos.jfiglet.FigletFont;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import java.io.IOException;
 import java.util.UUID;
 
 @SpringBootApplication
-public class App implements CommandLineRunner {
+public class App {
 
     @Autowired
     private UserRepository userRepository;
 
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
-        System.out.println(UUID.randomUUID());
+
+        SpringApplication app = new SpringApplication(App.class);
+        app.setBanner((environment, sourceClass, out) -> {
+            String fontName = "<<H@ND3VCOD3R>>";
+            String asciiArt = null;
+            try {
+                asciiArt = FigletFont.convertOneLine(fontName);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            out.println(asciiArt);
+        });
+        app.setBannerMode(Banner.Mode.CONSOLE);
+        app.run(args);
+//        System.out.println(UUID.randomUUID());
     }
 
-    @Override
-    public void run(String... args) throws Exception {
-        User adminAccount = userRepository.findByRole(Role.ADMIN);
-        if (null == adminAccount) {
-            User user = new User();
-            user.setEmail("admin@email.com");
-            user.setFirstname("admin");
-            user.setLastname("admin");
-            user.setRole(Role.ADMIN);
-            user.setPassword(new BCryptPasswordEncoder().encode("admin"));
-            userRepository.save(user);
-        }
-
-    }
 }
