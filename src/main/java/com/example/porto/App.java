d0414b14ce66bafbee475ae3594a7b1aaa99b995
handevcoder <iwansyy@gmail.com>
2024-06-11 23:00:16 +0700

    - Add Figlet just for fun

--- src/main/java/com/example/porto/App.java
+++ src/main/java/com/example/porto/App.java
@@ -1,20 +1,14 @@
 package com.example.porto;
 
-import com.example.porto.mvc.repository.UserRepository;
 import com.github.lalyos.jfiglet.FigletFont;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.IOException;
-import java.util.UUID;
 
 @SpringBootApplication
 public class App {
-
-    @Autowired
-    private UserRepository userRepository;
 
     public static void main(String[] args) {
 
